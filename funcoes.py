df65f88f92941fce58fc5d89635e89a161ad53f4
Beto312 <robertomb4@al.insper.edu.br>
2025-10-20 17:25:21 -0300

    ex4

--- funcoes.py
+++ funcoes.py
@@ -7,6 +7,7 @@ def define_posicoes (linha, coluna, orientacao, tamanho):
         for i in range(tamanho):
             posicoes.append([linha, coluna + i])
     return posicoes
+
 def preenche_frota (frota, nome_navio, linha, coluna, orientacao, tamanho):
     posicoes = define_posicoes(linha, coluna, orientacao, tamanho)
     if nome_navio in frota:
@@ -20,4 +21,20 @@ def faz_jogada (tabuleiro, linha, coluna):
         tabuleiro[linha][coluna] = 'X'
     else: 
         tabuleiro[linha][coluna] = "-"
+    return tabuleiro
+
+def posiciona_frota (frota):
+    tabuleiro = []
+    for i in range(10):
+        linha = []
+        for j in range(10):
+            linha.append(0)
+        tabuleiro.append(linha)
+    for chave, valor in frota.items():
+        for i in range(len(valor)):
+            for j in range(len(valor[i])):
+                x = valor[i][j][0]
+                y = valor[i][j][1] 
+
+                tabuleiro[x][y] = 1
     return tabuleiro
